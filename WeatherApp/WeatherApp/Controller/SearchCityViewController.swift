d2627a91a712841f793917b529184f92251f39b8
Pavitra hegde <iampavitrahegde@gmail.com>
2020-08-15 13:30:50 +0530

    Implements Search UI

--- WeatherApp/WeatherApp/Controller/SearchCityViewController.swift
+++ WeatherApp/WeatherApp/Controller/SearchCityViewController.swift
@@ -11,25 +11,38 @@ import UIKit
 class SearchCityViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-    var searchController: UISearchController?
+    private let searchController = UISearchController()
+    private let geocoder = LocationGeocoder()
+    private var searchResult: [SearchResponse] = [] {
+        didSet {
+            self.tableView.reloadData()
+        }
+    }
+    var selectedSearchItem: SearchResponse
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        searchController?.searchBar.sizeToFit()
-        navigationItem.titleView = searchController?.searchBar
+        searchController.searchResultsUpdater = self
+        searchController.searchBar.showsCancelButton = false
+        navigationItem.titleView = searchController.searchBar
+        searchController.hidesNavigationBarDuringPresentation = false
         definesPresentationContext = true
-        searchController?.hidesNavigationBarDuringPresentation = false
+    }
+    
+    @IBAction func cancelButtonTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
     }
 }
 
 extension SearchCityViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return searchResult.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let searchItem = searchResult[indexPath.row]
+        cell.textLabel?.text = searchItem.displayName
         return cell
     }
     
@@ -37,5 +50,29 @@ extension SearchCityViewController: UITableViewDataSource {
 }
 
 extension SearchCityViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.selectedSearchItem = self.searchResult[indexPath.row]
+        performSegue(withIdentifier: "didSelectCity", sender: nil)
+        
+    }
+}
+
+extension SearchCityViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchText = searchController.searchBar.text
+        guard searchText?.isEmpty == false else {
+            return
+        }
+        geocoder.startGeocoding(query: searchText!) { (error, response) in
+            
+            if let error = error {
+                print(error)
+            } else {
+                self.searchResult = response ?? []
+            }
+        }
+    }
+    
     
 }
